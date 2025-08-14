1ba0613ce2e369dffeb7197071879bc2e9bd0aee
semonLuo <1804448577@qq.com>
2025-08-14 11:52:51 +0800

    feat: upd some go file

--- Notes/opengl.cpp
+++ Notes/opengl.cpp
@@ -1,16 +1,35 @@
+// 对于GLEW，需要在包含其他OpenGL头文件前定义GLEW_STATIC（如果是静态链接）
+#ifdef __MINGW32__
+#define GLEW_STATIC
+#endif
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
-    
-    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
     if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     
     glfwMakeContextCurrent(window);
+    
+    // 初始化GLEW
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(err) << std::endl;
+        return -1;
+    }
+    
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     
     while (!glfwWindowShouldClose(window)) {
         // 渲染代码
